baa9bdb9f90f912851fb76cecbe53bc9d84fcf79
anniez32 <113133885+anniez32@users.noreply.github.com>
2024-10-06 20:23:06 -0400

    Update Zhao_Annie_Homework_2_Part_2.cpp

--- Zhao_Annie_Homework_2_Part_2.cpp
+++ Zhao_Annie_Homework_2_Part_2.cpp
@@ -8,50 +8,66 @@ using namespace std;
 main()
 {
     // Variables
-    float j1;
-    float j2;
-    float j3;
-    float j4;
-    float j5;
-    float j6;
-    double score;  // final score
+    double j1;
+    double j2;
+    double j3;
+    double j4;
+    double j5;
+    double j6;
+    double smallest = 10;
+    double largest = 0;
     double total;
+    double average;  // final score average
 
     // Ask for input
-    cout << "What are the 6 scores given? (Separate each score with a space.) :";
+    cout << "What are the 6 scores given? (Seperate each score with a space.) : ";
     cin >> j1 >> j2 >> j3 >> j4 >> j5 >> j6;
 
-    // Calculating the total
+    // Removing the largest number
+    if (j1 >= largest)
+        largest = j1;
+    if (j2 >= largest)
+        largest = j2;
+    if (j3 >= largest)
+        largest = j3;
+    if (j3 >= largest)
+        largest = j3;
+	if (j4 >= largest)
+        largest = j4;
+	if (j5 >= largest)
+        largest = j5;
+	if (j6 >= largest)
+        largest = j6;
 
-    total = j1 + j2 + j3 + j4 + j5 + j6;
+    // Removing the smallest number
+	if (j1 <= smallest)
+		smallest = j1;
+	if (j2 <= smallest)
+		smallest = j2;
+	if (j3 <= smallest)
+		smallest = j3;
+	if (j4 <= smallest)
+		smallest = j4;
+	if (j5 <= smallest)
+		smallest = j5;
+	if (j6 <= smallest)
+		smallest = j6;
 
-    // Removing the highest number
-    if (j1 > j2 && j3 && j4 && j5 && j6)
-        total - j1;
-    else if (j2 > j1 && j3 && j4 && j5 && j6)
-        total - j2;
-    else if (j3 > j2 && j1 && j4 && j5 && j6)
-        total - j3;
-    else if (j4 > j2 && j3 && j1 && j5 && j6)
-        total - j4;
-    else if (j5 > j2 && j3 && j4 && j1 && j6)
-        total - j5;
-    else if (j6 > j2 && j3 && j4 && j5 && j1)
-        total - j6;
+	// Calculating the total
+	total = j1 + j2 + j3 + j4 + j5 + j6 - largest - smallest;
 
-    // Removing the lowest number
-    if (j1 < j2 && j3 && j4 && j5 && j6)
-        total - j1;
-    else if (j2 < j1 && j3 && j4 && j5 && j6)
-        total - j2;
-    else if (j3 < j2 && j1 && j4 && j5 && j6)
-        total - j3;
-    else if (j4 < j2 && j3 && j1 && j5 && j6)
-        total - j4;
-    else if (j5 < j2 && j3 && j4 && j1 && j6)
-        total - j5;
-    else if (j6 < j2 && j3 && j4 && j5 && j1)
-        total - j6;
+	// Calculating the average
+	average = total/4;
 
-    cout << j1 << j2 << j3 << j4 << j5 << j6 << total;
+	// Output
+	
+	cout << "Final Score : " << average << endl;
+
+	return 0;
 }
+
+// Output Message
+/*
+What are the 6 scores given? (Separate each score with a space.) : 1 2 3 4 5 6
+Final Score: 3.5
+*/
